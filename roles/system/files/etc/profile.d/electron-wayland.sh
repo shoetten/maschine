486b3de95873662b3fc6f9b9bf5cf288731c629e
Simon Hötten <s+git@hoetten.org>
2024-03-17 19:05:31 +0100

    enable wayland for electron>=28

---
+++ roles/system/files/etc/profile.d/electron-wayland.sh
@@ -0,0 +1,3 @@
+# Use wayland for electron, see
+# https://github.com/electron/electron/blob/main/docs/api/environment-variables.md#electron_ozone_platform_hint-linux
+export ELECTRON_OZONE_PLATFORM_HINT="auto"
